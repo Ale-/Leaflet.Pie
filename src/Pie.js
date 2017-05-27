3097821efdae0ef1a2f606d6efd3c71e1e7d0c2c
Ale <a@414c45.net>
2017-05-27 16:07:40 +0100

    refactor: Move function to draw the pie to Util.js

--- src/Pie.js
+++ src/Pie.js
@@ -18,29 +18,8 @@ L.Pie.Icon = L.DivIcon.extend(
         options.data_sum   = options.data.reduce( (x,y) => x+y );
         const pie_diameter = 2 * Math.floor( options.dataset.calculate_radius(options.data_sum) );
         options.iconSize   = [ pie_diameter, pie_diameter ]
-        options.html       = this._drawPie(pie_diameter, options);
+        options.html       = L.Pie.Util.drawPie(pie_diameter, options);
         L.setOptions(this, options);
-    },
-
-    /** Returns the markup of the Icon. **/
-    _drawPie(diameter, options){
-        var markup =  "<p class='leaflet-marker-icon__label'>" + options.label + "</p><svg class='leaflet-marker-icon__content leaflet-svg' width=" + diameter + " height=" + diameter + " viewBox='0 0 2 2'>";
-        let angle = 0;
-        let x = 1 + Math.cos(angle);
-        let y = 1 + Math.sin(angle);
-        for(var i = 0; i < options.data.length; i++){
-            const angle_increment = options.data[i] * Math.PI * 2 / options.data_sum;
-            const large_flag      = angle_increment > Math.PI ? 1 : 0;
-            let path  = "<path class='leaflet-svg__arc leaflet-svg__arc--" + L.Pie.Util.slugify(options.dataset.categories[i].name) + "' fill='" + options.dataset.categories[i].col  + "' d='M " + x + " " + y + " A 1 1 0 ";
-            angle    += angle_increment;
-            x         = 1 + Math.cos(angle);
-            y         = 1 + Math.sin(angle);
-            path     += large_flag + " 1 " + x + " " + y + " L 1 1 Z' />";
-            markup   += path;
-        }
-        markup += "</svg>";
-
-        return markup;
     },
 
 });
--- src/Util.js
+++ src/Util.js
@@ -3,7 +3,7 @@
  * @param {string} text - string to be slugified
  * @see https://gist.github.com/mathewbyrne/1280286
  */
- 
+
 L.Pie.Util.slugify = function(text){
     return text.toString().toLowerCase()
         .replace(/\s+/g, '-')      // Replace spaces with -
@@ -11,4 +11,25 @@ L.Pie.Util.slugify = function(text){
         .replace(/\-\-+/g, '-')    // Replace multiple - with single -
         .replace(/^-+/, '')        // Trim - from start of text
         .replace(/-+$/, '');       // Trim - from end of text
+};
+
+/** Returns the markup of the Icon. **/
+L.Pie.Util.drawPie = function(diameter, options, className){
+    var markup = "<svg class='" + className + "' width=" + diameter + " height=" + diameter + " viewBox='0 0 2 2'>";
+    let angle = 0;
+    let x = 1 + Math.cos(angle);
+    let y = 1 + Math.sin(angle);
+    for(var i = 0; i < options.data.length; i++){
+        const angle_increment = options.data[i] * Math.PI * 2 / options.data_sum;
+        const large_flag      = angle_increment > Math.PI ? 1 : 0;
+        let path  = "<path class='" + className + "__dataitem' fill='" + options.dataset.categories[i].col  + "' d='M " + x + " " + y + " A 1 1 0 ";
+        angle    += angle_increment;
+        x         = 1 + Math.cos(angle);
+        y         = 1 + Math.sin(angle);
+        path     += large_flag + " 1 " + x + " " + y + " L 1 1 Z' />";
+        markup   += path;
+    }
+    markup += "</svg>";
+
+    return markup;
 };
